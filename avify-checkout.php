c7d217e61c69136aa270af5443d32b3640dcbfea
Pablo Gutierrez <jpgutierrez07.pg@gmail.com>
2023-12-04 17:46:15 -0600

    Avify checkout show multiple totals

--- avify-checkout.php
+++ avify-checkout.php
@@ -2,7 +2,7 @@
 function load_avify_checkout_scripts() {
     $options = get_option('avify-settings-options');
     if (($options['avify_enable_checkout'] ?? '') === 'on' && is_checkout()) {
-        $v = '2.4';
+        $v = '2.5';
         wp_enqueue_script('avify-checkout', plugin_dir_url( __FILE__ ) . '/assets/avify-checkout.js', array('jquery', 'jquery-ui-sortable', 'jquery-ui-resizable'), $v);
         wp_enqueue_style('avify-checkout', plugin_dir_url( __FILE__ ) . '/assets/avify-checkout.css', false, $v);
     }
