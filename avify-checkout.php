f402621c308188b847a8a434a8234b295c78ab1e
Pablo Gutierrez <jpgutierrez07.pg@gmail.com>
2026-02-12 14:11:42 -0600

    Add country selector for phone input

--- avify-checkout.php
+++ avify-checkout.php
@@ -2,7 +2,7 @@
 function load_avify_checkout_scripts() {
     $options = get_option('avify-settings-options');
     if (($options['avify_enable_checkout'] ?? '') === 'on' && is_checkout()) {
-        $v = '1.3.7';
+        $v = '1.3.8';
         wp_enqueue_script('avify-checkout', plugin_dir_url( __FILE__ ) . '/assets/avify-checkout.js', array('jquery', 'jquery-ui-sortable', 'jquery-ui-resizable'), $v);
         wp_enqueue_style('avify-checkout', plugin_dir_url( __FILE__ ) . '/assets/avify-checkout.css', false, $v);
 
